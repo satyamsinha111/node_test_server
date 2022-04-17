90523678458f82869bcd133ad19515904ac11b86
satyamsinha111 <satyamsinha8158@gmail.com>
2022-04-17 22:10:59 +0530

    added start script

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const PORT = 3000 | process.env.PORT;
 app.get("/", (req, res) => {
   res.send("Helloworld to devops");
 });
 
-app.listen(3000 | process.env.PORT, () => {
-  console.log("Listning to server");
+app.listen(PORT, () => {
+  console.log("Listning to server at " + PORT);
 });
