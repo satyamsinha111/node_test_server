7746e89398779f938802a5056ba4ae7a83bb834e
satyamsinha111 <satyamsinha8158@gmail.com>
2022-04-19 05:46:08 +0530

    pushed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000 | process.env.PORT;
+const PORT = 80 | process.env.PORT;
 app.get("/", (req, res) => {
   res.send("Helloworld to devops");
 });
